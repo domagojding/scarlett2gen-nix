23266a7e949d9ecca44db6414db5ed99b63ad119
stablejoy <dooygoy@gmail.com>
2024-04-26 12:21:12 +0200

    new iteration that gives error

--- scarlett.nix
+++ scarlett.nix
@@ -3,6 +3,8 @@
   lib, 
   stdenv,
   fetchFromGitHub,
+  kernel,
+  kmod
 }:
 
 stdenv.mkDerivation {
@@ -14,5 +16,23 @@ stdenv.mkDerivation {
     repo = "scarlett-gen2";
     rev = "v1.0";
     sha256 = "0208pxyr2rhcb4lz191m3zj482pmlkamcqbjx4adlazv0pg8ivyx";
-    };
+  };
+
+  hardeningDisable = [ "pic" "format" ];
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+
+  makeFlags = [
+    "KVERSION=${kernel.modDirVersion}"
+    "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "MODULEDIR=$(out)"
+  ];
+
+  meta = with lib; {
+    description = "Linux kernel driver for Focusrite Scarlett 2nd Generation interfaces";
+    homepage = "https://github.com/geoffreybennett/scarlett-gen2";
+    license = licenses.gpl2Only;
+    maintainers = [ maintainers.yourname ];
+    platforms = platforms.linux;
+  };
+
 }
